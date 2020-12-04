50a2eecb84d8676174e0f61ad9b417737e94b4e6
remi.vezy <VEZY@users.noreply.github.com>
2020-12-04 15:15:10 +0100

    Update tests (added one parameter -> TMR_Tree)

--- test/parameters.jl
+++ test/parameters.jl
@@ -4,6 +4,6 @@ using Test
 @testset "parameters" begin
     @test read_param_file(:constants,"package") == constants()
     a= import_parameters("package")
-    @test length(a) == 207
+    @test length(a) == 208
     @test a.cp == constants().cp
 end;
